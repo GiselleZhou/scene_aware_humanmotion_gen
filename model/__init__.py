6198fd2c0ab7906cd938dff73e54439d890a9029
GiselleZhou <zixuanzhou816@gmail.com>
2025-05-15 09:48:14 +0100

    All the project connected files

---
+++ model/__init__.py
@@ -0,0 +1,4 @@
+from .mdm import MDM
+from .DoubleTake_MDM import doubleTake_MDM
+
+__all__ = ["MDM", "doubleTake_MDM"]
---
+++ train/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="train-platforms",
+    version="0.1",
+    packages=find_packages(),      # will pick up train_platforms.py
+)
